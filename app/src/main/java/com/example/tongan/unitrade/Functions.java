5c10de62b58c7c5f65b3dcb758420545a54949a0
Tong An <an82@purdue.edu>
2018-09-22 20:10:50 -0400

    functions file

---
+++ app/src/main/java/com/example/tongan/unitrade/Functions.java
@@ -0,0 +1,28 @@
+package com.example.tongan.unitrade;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class Functions {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+    //AT!!
+    void change_notification(String username, int setting){
+
+
+
+    }
+
+
+
+    //Mia!!
+
+
+
+
+    //Scott!!
+
+
+
+
+}

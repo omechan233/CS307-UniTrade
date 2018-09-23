14cdabbdf65e95292a79d06b93bdb9085004ec13
Yuening Xu <xu_830@outlook.com>
2018-09-23 00:18:33 -0400

    wishlist add and remove, delete post Yuening Xu

--- app/src/main/java/com/example/tongan/unitrade/Functions.java
+++ app/src/main/java/com/example/tongan/unitrade/Functions.java
@@ -17,9 +17,32 @@ public class Functions {
 
 
     //Mia!!
-    //use item id to get item, return 0 if it has been removed successfully,
-    //return 1 if do not success.
-    int delete_item(int itemid, int wishlistid){
+
+    /*
+    delete item from post
+    use item id to get item, return 0 if it has been removed successfully,
+    return 1 if do not success.
+     */
+    int delete_post(int itemid, int postid){
+        return 0;
+    }
+
+    /*
+    delete item from wishlist
+    use item id to get item, return 0 if it has been removed successfully,
+    return 1 if do not success.
+    */
+    int delete_wishlist(int itemid, int wishlistid){
+        return 0;
+    }
+
+    /*
+    add item to wishlist
+    use item id to get item, return 0 if it has been added successfully,
+    return 1 if do not success.
+    */
+
+    int add_wishlist(int itemid, int wishlistid){
         return 0;
     }
 

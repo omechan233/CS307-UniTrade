d5e6aea799b391dcb580777d3f05b8377fd853ba
Yancheng Guo <guo361@purdue.edu>
2018-10-02 20:06:56 -0400

    add create new post method in NewPostActivity

--- app/src/main/java/com/example/tongan/unitrade/NewPostActivity.java
+++ app/src/main/java/com/example/tongan/unitrade/NewPostActivity.java
@@ -37,13 +37,29 @@ public class NewPostActivity extends AppCompatActivity {
                 //get input in edittext
                 String itemName = itemName_edit.getText().toString();
                 String description = description_edit.getText().toString();
+                String username = "";
+                String postedtime = "";
+                String address ="";
+                String category = "";
                 Double price = -1.0;
+
+                // 0 for currently unavailable
+                // 1 for available
+                // 2 for someone bought it
+                int status = 2;
+                Functions f = new Functions();
+                MainActivity mainActivity = new MainActivity();
+                String email = mainActivity.getEmail();
+                //get username by email
+                username = f.get_username_by_email(email);
 
                 //check if the user input is empty
                 if (!itemName.equals("") && !description.equals("") && !price_edit.getText().toString().equals("")) {
 
                     price = Double.parseDouble(price_edit.getText().toString());
                     //todo : store the itemName, description and price to database here
+
+                    f.create_post(itemName, username, postedtime, price,category,address, description,status);
 
                     //back to homepage
                     Toast.makeText(getBaseContext(), "Success!", Toast.LENGTH_LONG).show();

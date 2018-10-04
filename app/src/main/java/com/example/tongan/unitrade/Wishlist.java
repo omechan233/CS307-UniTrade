ba7e2c85865e57501c36190f5b94508f1d78e0b0
omechan233 <31775045+omechan233@users.noreply.github.com>
2018-10-04 04:53:42 -0400

    Add files via upload

--- app/src/main/java/com/example/tongan/unitrade/Wishlist.java
+++ app/src/main/java/com/example/tongan/unitrade/Wishlist.java
@@ -7,7 +7,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.example.tongan.unitrade.objects.Item;
@@ -28,8 +31,44 @@ public class Wishlist extends AppCompatActivity {
 
         Button homebtn = (Button) findViewById(R.id.wishlist_home_btn);
         Button profilebtn = (Button) findViewById(R.id.wishlist_profile_btn);
-        populateWishitem();
-        populateWishlistview();
+
+        Item test1 = new Item("Test Category1", "POI", "Yudachi", "1996-5-5", 2.33,
+                "This is the content of description\nYou don't need to look at it at all\nsince this description is totally meaningless!\nlol", "ass", 0);
+        Item test2 = new Item("Test Category2", "POII", "Yudachi_2", "1996-5-6", 3.33,
+                "Yep, this description is still meaningless!\nthe good thing is that it is shorter than previous one", "ass", 0);
+        Item test3 = new Item("Test Category3", "POIII", "Yudachi_3", "1996-5-7", 4.33,
+                "Emmmm.......\nI have to write something here for test right?", "ass", 0);
+        final ArrayList<Item> test_list = new ArrayList<Item>();
+        test_list.add(test1);
+        test_list.add(test2);
+        test_list.add(test3);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.wlistview);
+        linearLayout.removeAllViews();
+        for (int i = 0; i < test_list.size(); i++) {
+            LinearLayout item = new LinearLayout(getBaseContext());
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(400, 200);
+            item.setLayoutParams(params);
+            ImageView imageView = new ImageView(getBaseContext());
+            imageView.setImageResource(R.mipmap.poi_test_src);
+            params = new LinearLayout.LayoutParams(180, 180);
+            params.setMargins(20, 20, 0, 20);
+            imageView.setLayoutParams(params);
+            item.addView(imageView);
+            TextView tv = new TextView(getBaseContext());
+            //todo : the String below is getting information from a hard coding ArrayList<Item>, change it to adapt the actual data retrieved from backend
+            String text = "\n" + test_list.get(i).getTitle() + "\n" + test_list.get(i).getPrice() + "\n" + test_list.get(i).getSeller_name();
+            tv.setText(text);
+            item.addView(tv);
+            item.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //todo:get the item_id of the selected item and store it into a global variable that can be used in the ItemDetail page(need to know which item to display detail)
+                    startActivity(new Intent(Wishlist.this, ItemDetail.class));
+                }
+            });
+            linearLayout.addView(item);
+        }
 
         homebtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,59 +87,5 @@ public class Wishlist extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-    }
-    private void populateWishitem() {
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        //get a list of wishitem id
-        List<String> wishitem_id = new ArrayList<>();
-        wishitem_id = f1.get_itemid_list(currentUser.getUid());
-        //use id to get a list of item
-        wishlist = f1.get_wanted_item(wishitem_id);
-    }
-
-    private void populateWishlistview() {
-        //build adapter
-        ArrayAdapter<Item> adapter = new wishlistAdapter();
-        //configure list view
-        ListView wishlist = (ListView) findViewById(R.id.wlistview);
-        wishlist.setAdapter(adapter);
-    }
-    private class wishlistAdapter extends ArrayAdapter<Item> {
-        public wishlistAdapter(){
-            super(Wishlist.this, R.layout.wishlistitem,wishlist);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent){
-            //make sure we have a view to work with
-            View itemView = convertView;
-            if (itemView == null){
-                itemView = getLayoutInflater().inflate(R.layout.wishlistitem, parent, false);
-            }
-
-            //find the item to work with
-            Item currentItem = wishlist.get(position);
-
-            //fill the view
-
-            //show pictures of item
-            //ImageView imageView = (ImageView) itemView.findViewById(R.id.itempic);
-            //imageView.setImageResource(currentItem.getIconId());
-
-            //set title
-            TextView titleText = (TextView) itemView.findViewById(R.id.item_name);
-            titleText.setText(currentItem.getTitle());
-
-            //set seller
-            TextView sellerText = (TextView) itemView.findViewById(R.id.seller);
-            sellerText.setText(currentItem.getSeller_name());
-
-            //set price
-            TextView priceText = (TextView) itemView.findViewById(R.id.item_price);
-            sellerText.setText(Double.toString(currentItem.getPrice()));
-
-            return itemView;
-        }
     }
 }

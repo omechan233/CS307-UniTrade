99e3706ec353770616044af26d61f7a286125e7d
Tong An <an82@purdue.edu>
2018-10-21 17:59:11 -0400

    Updated Order class

--- app/src/main/java/com/example/tongan/unitrade/objects/Order.java
+++ app/src/main/java/com/example/tongan/unitrade/objects/Order.java
@@ -4,115 +4,48 @@ import java.util.Date;
 
 
 public class Order {
-    private int ID;
-    private int PostID;
-    private int BuyerID;
-    private int SellerID;
-    private Date dateOfPurchase;
+    private String item_title;
+    private Double item_price;
+    private boolean is_sold;
+    private String buyer_email;
+    private String item_ID;
+    private String order_time;
 
     /**
      * Constructor; Creates an order based on a purchased Item
-     * 
-     * @param ID Order's ID
-     * @param PostID Post's ID
-     * @param BuyerID Buyer's UserID
-     * @param SellerID Seller's UserID
-     * @param dateOfPurchase Date when the Item was purchased
+     *
      */
-    public Order(int ID, int PostID, int BuyerID, int SellerID, Date dateOfPurchase){
-        this.ID = ID;
-        this.PostID = PostID;
-        this.BuyerID = BuyerID;
-        this.SellerID = SellerID;
-        this.dateOfPurchase = dateOfPurchase;
+    public Order(){}
+    public Order(String item_ID, String order_time, String buyer_email, String item_title, Double item_price, boolean is_sold){
+        this.buyer_email=buyer_email;
+        this.is_sold=is_sold;
+        this.item_ID=item_ID;
+        this.item_price=item_price;
+        this.item_title=item_title;
+        this.order_time=order_time;
     }
 
-    /**
-     * Getter method for Order's ID
-     * @return Order ID
-     */
-    public int GetID(){
-        return this.ID;
+    public Double getItem_price() {
+        return item_price;
     }
 
-    /**
-     * Getter method for Order's Post's ID
-     * 
-     * @return Post ID
-     */
-    public int getPostID(){
-        return this.PostID;
-    }
-    
-    /**
-     * Getter method for buyer's UserID
-     * 
-     * @return buyer's UserID
-     */
-    public int getBuyerID(){
-        return this.BuyerID;
+    public boolean isIs_sold() {
+        return is_sold;
     }
 
-    /**
-     * Getter method for seller's UserID
-     * 
-     * @return seller's UserID
-     */
-    public int getSellerID(){
-        return this.SellerID;
+    public String getBuyer_email() {
+        return buyer_email;
     }
 
-    /**
-     * Getter method for the Order's date of purchase
-     * 
-     * @return Order's date of purchase
-     */
-    public Date getDateOfPurchase(){
-        return this.dateOfPurchase;
+    public String getItem_ID() {
+        return item_ID;
     }
 
-    /**
-     * Setter method for Order's ID
-     * 
-     * @param ID new Order ID
-     */
-    public void setID(int ID){
-        this.ID = ID;
+    public String getItem_title() {
+        return item_title;
     }
 
-    /**
-     * Setter method for Order's Post's ID
-     * 
-     * @param PostID new Post ID
-     */
-    public void setPostID(int PostID){
-        this.PostID = PostID;
-    }
-
-    /**
-     * Setter method for Order's Buyer's ID
-     * 
-     * @param BuyerID new Buyer ID
-     */
-    public void setBuyerID(int BuyerID){
-        this.BuyerID = BuyerID;
-    }
-
-    /**
-     * Setter method for Order's Seller's ID
-     * 
-     * @param SellerID new Seller ID
-     */
-    public void setSellerID(int SellerID){
-        this.SellerID = SellerID;
-    }
-
-    /**
-     * Setter method for the Order's date of purchase
-     * 
-     * @param date new date of purchase
-     */
-    public void setDateOfPurchase(Date date){
-        this.dateOfPurchase = date;
+    public String getOrder_time() {
+        return order_time;
     }
 }

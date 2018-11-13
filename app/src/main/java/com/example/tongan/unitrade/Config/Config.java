be7ce98f117b682f2b44ed837ed72b3fc8ff5c47
Yancheng Guo <guo361@purdue.edu>
2018-11-13 18:42:29 -0500

    add Config.java

---
+++ app/src/main/java/com/example/tongan/unitrade/Config/Config.java
@@ -0,0 +1,5 @@
+package com.example.tongan.unitrade.Config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID ="AZy6mX1mTKO8Qaxc4CP0FeEk53lfxGriUCJmwTsRscof9vPkoe55epXwiRZ9_D14Op5QnMe08Y1Riol7";
+}

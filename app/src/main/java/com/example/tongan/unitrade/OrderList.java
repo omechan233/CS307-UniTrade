fbf9a5256aee064b1abc86d17769c40f292a0020
Scott Merritt <smerrit@purdue.edu>
2018-11-13 20:48:08 -0500

    Added image functionality to My Order/Item List Page

--- app/src/main/java/com/example/tongan/unitrade/OrderList.java
+++ app/src/main/java/com/example/tongan/unitrade/OrderList.java
@@ -3,6 +3,7 @@ package com.example.tongan.unitrade;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,13 +18,19 @@ import android.widget.Toast;
 import com.example.tongan.unitrade.objects.Item;
 import com.example.tongan.unitrade.objects.Order;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,6 +38,7 @@ public class OrderList extends AppCompatActivity {
     private static final String TAG = "OrderList";
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private SharedPreferences shared;
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
 
 
     @Override
@@ -83,22 +91,72 @@ public class OrderList extends AppCompatActivity {
                                         item_doc.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                                             @Override
                                             public void onSuccess(DocumentSnapshot documentSnapshot) {
-                                                Order current_order = documentSnapshot.toObject(Order.class);
+                                                final Order current_order = documentSnapshot.toObject(Order.class);
 
-                                                LinearLayout item = new LinearLayout(getBaseContext());
+                                                final LinearLayout item = new LinearLayout(getBaseContext());
                                                 LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(400, 200);
                                                 item.setLayoutParams(params);
-                                                ImageView imageView = new ImageView(getBaseContext());
-                                                imageView.setImageResource(R.mipmap.poi_test_src);
-                                                params = new LinearLayout.LayoutParams(180, 180);
-                                                params.setMargins(20, 20, 0, 20);
-                                                imageView.setLayoutParams(params);
-                                                item.addView(imageView);
-                                                TextView tv = new TextView(getBaseContext());
 
-                                                String text = "\n" + current_order.getItem_title() + "\n" + current_order.getItem_price() + "\n" + current_order.getSeller_email() + "\n" + current_order.getOrder_time() + "\n" + current_order.getFace_to_face();
-                                                tv.setText(text);
-                                                item.addView(tv);
+                                                //get item's image from storage
+                                                StorageReference storageRef = storage.getReference();
+                                                String picPath = documentSnapshot.getString("item_image");
+
+                                                StorageReference picRef = null;
+                                                if(picPath != null)
+                                                    picRef = storageRef.child(picPath);
+
+                                                if(picRef != null){
+                                                    try{
+                                                        //TODO: add logic to allow for different file types
+                                                        final File localFile = File.createTempFile("Images", "jpg");
+                                                        if(localFile != null) {
+                                                            picRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                                                                @Override
+                                                                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                                                                    //create and set params of image in parent layout
+                                                                    ImageView imageView = new ImageView(getBaseContext());
+                                                                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(180, 180);
+                                                                    params.setMargins(20, 20, 0, 20);
+                                                                    imageView.setLayoutParams(params);
+                                                                    item.addView(imageView);
+                                                                    imageView.setImageBitmap(BitmapFactory.decodeFile(localFile.getAbsolutePath()));
+
+
+                                                                    params = new LinearLayout.LayoutParams(180, 180);
+                                                                    params.setMargins(20, 20, 0, 20);
+                                                                    TextView tv = new TextView(getBaseContext());
+                                                                    //todo : the String below is getting information from a hard coding ArrayList<Item>, change it to adapt the actual data retrieved from backend
+                                                                    String text = "\n" + current_order.getItem_title() + "\n" + current_order.getItem_price();
+                                                                    tv.setText(text);
+                                                                    item.addView(tv);
+                                                                }
+                                                            }).addOnFailureListener(new OnFailureListener() {
+                                                                @Override
+                                                                public void onFailure(@NonNull Exception e) {
+                                                                    System.out.println("Something went wrong with getting the profile image! Message: " + e.getLocalizedMessage());
+                                                                }
+                                                            });
+                                                        }else
+                                                            Log.e(TAG, "Improper File Type!");
+                                                    }catch(IOException e) {
+                                                        Log.e(TAG, "IOError attempting to get image from Storage, message: " + e.getLocalizedMessage());
+                                                    }
+                                                }
+                                                else{
+                                                    //create and set params of image in parent layout
+                                                    ImageView imageView = new ImageView(getBaseContext());
+                                                    imageView.setImageResource(R.mipmap.poi_test_src);
+                                                    params = new LinearLayout.LayoutParams(180, 180);
+                                                    params.setMargins(20, 20, 0, 20);
+                                                    imageView.setLayoutParams(params);
+                                                    item.addView(imageView);
+
+                                                    //create textview in parent layout
+                                                    TextView tv = new TextView(getBaseContext());
+                                                    String text = "\n" + current_order.getItem_title() + "\n" + current_order.getItem_price() + "\n" + current_order.getSeller_email() + "\n" + current_order.getOrder_time() + "\n" + current_order.getFace_to_face();
+                                                    tv.setText(text);
+                                                    item.addView(tv);
+                                                }
                                                 final Order finalCurrent_Order = current_order;
                                                 item.setOnClickListener(new View.OnClickListener() {
                                                     @Override
@@ -158,27 +216,77 @@ public class OrderList extends AppCompatActivity {
                             } else {
                                 for (int i = 0; i < my_items.size(); i++) {
                                     final DocumentReference item_doc = db.collection("items").document(my_items.get(i));
-                                    final int finalI = i;
                                     item_doc.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                                         @Override
                                         public void onSuccess(DocumentSnapshot documentSnapshot) {
-                                            Item current_item = new Item();
-                                            current_item = documentSnapshot.toObject(Item.class);
+                                            final Item current_item = documentSnapshot.toObject(Item.class);
+                                            final LinearLayout item = new LinearLayout(getBaseContext());
 
-                                            LinearLayout item = new LinearLayout(getBaseContext());
                                             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(400, 200);
                                             item.setLayoutParams(params);
-                                            ImageView imageView = new ImageView(getBaseContext());
-                                            imageView.setImageResource(R.mipmap.poi_test_src);
-                                            params = new LinearLayout.LayoutParams(180, 180);
-                                            params.setMargins(20, 20, 0, 20);
-                                            imageView.setLayoutParams(params);
-                                            item.addView(imageView);
-                                            TextView tv = new TextView(getBaseContext());
-                                            //todo : the String below is getting information from a hard coding ArrayList<Item>, change it to adapt the actual data retrieved from backend
-                                            String text = "\n" + current_item.getTitle() + "\n" + current_item.getPrice() + "\n" + current_item.getSeller_name();
-                                            tv.setText(text);
-                                            item.addView(tv);
+
+                                            //get item's image from storage
+                                            StorageReference storageRef = storage.getReference();
+                                            String picPath = documentSnapshot.getString("item_image");
+
+                                            StorageReference picRef = null;
+                                            if(picPath != null)
+                                                picRef = storageRef.child(picPath);
+
+                                            if(picRef != null){
+                                                try{
+                                                    //TODO: add logic to allow for different file types
+                                                    final File localFile = File.createTempFile("Images", "jpg");
+                                                    if(localFile != null) {
+                                                        picRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                                                            @Override
+                                                            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                                                                //create and set params of image in parent layout
+                                                                ImageView imageView = new ImageView(getBaseContext());
+                                                                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(180, 180);
+                                                                params.setMargins(20, 20, 0, 20);
+                                                                imageView.setLayoutParams(params);
+                                                                item.addView(imageView);
+                                                                imageView.setImageBitmap(BitmapFactory.decodeFile(localFile.getAbsolutePath()));
+
+
+                                                                params = new LinearLayout.LayoutParams(180, 180);
+                                                                params.setMargins(20, 20, 0, 20);
+                                                                TextView tv = new TextView(getBaseContext());
+                                                                //todo : the String below is getting information from a hard coding ArrayList<Item>, change it to adapt the actual data retrieved from backend
+                                                                String text = "\n" + current_item.getTitle() + "\n" + current_item.getPrice() + "\n" + current_item.getSeller_name();
+                                                                tv.setText(text);
+                                                                item.addView(tv);
+                                                            }
+                                                        }).addOnFailureListener(new OnFailureListener() {
+                                                            @Override
+                                                            public void onFailure(@NonNull Exception e) {
+                                                                System.out.println("Something went wrong with getting the profile image! Message: " + e.getLocalizedMessage());
+                                                            }
+                                                        });
+                                                    }else
+                                                        Log.e(TAG, "Improper File Type!");
+                                                }catch(IOException e) {
+                                                    Log.e(TAG, "IOError attempting to get image from Storage, message: " + e.getLocalizedMessage());
+                                                }
+                                            }
+                                            else{
+                                                //create and set params of image in parent layout
+                                                ImageView imageView = new ImageView(getBaseContext());
+                                                imageView.setImageResource(R.mipmap.poi_test_src);
+                                                params = new LinearLayout.LayoutParams(180, 180);
+                                                params.setMargins(20, 20, 0, 20);
+                                                imageView.setLayoutParams(params);
+                                                item.addView(imageView);
+
+                                                //create textview in parent layout
+                                                TextView tv = new TextView(getBaseContext());
+                                                String text = "\n" + current_item.getTitle() + "\n" + current_item.getPrice() + "\n" + current_item.getSeller_name();
+                                                tv.setText(text);
+                                                item.addView(tv);
+                                            }
+
+                                            //Connect each item to it's detail page
                                             final Item finalCurrent_item = current_item;
                                             item.setOnClickListener(new View.OnClickListener() {
                                                 @Override

53ebcb68b488ad2e445350c02491f86b973ffbe1
Tong An <an82@purdue.edu>
2018-11-24 14:47:24 -0500

    Fixed null pointer bug in nearby item list, also only displaying available items now

--- app/src/main/java/com/example/tongan/unitrade/NearbyItem.java
+++ app/src/main/java/com/example/tongan/unitrade/NearbyItem.java
@@ -61,101 +61,83 @@ public class NearbyItem extends AppCompatActivity {
         });
 
         Location location = getLastBestLocation();
-        final double final_lat = location.getLatitude();
-        final double final_lon = location.getLongitude();
-        System.out.println("coordinates: " + final_lat + " " + final_lon);
 
-        final LinearLayout items = (LinearLayout)findViewById(R.id.nearby_results);
+        if(location!=null) {
+            final double final_lat = location.getLatitude();
+            final double final_lon = location.getLongitude();
+            System.out.println("coordinates: " + final_lat + " " + final_lon);
 
-        db.collection("items")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            ArrayList<Item> items_list = new ArrayList<>();
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + " => " + document.getData());
-                                Item item = document.toObject(Item.class);
-                                if(item.getLatitude()!=0 && item.getLongitude()!=0){
-                                    items_list.add(item);
-                                }
-                            }
+            final LinearLayout items = (LinearLayout) findViewById(R.id.nearby_results);
 
-                            Comparator comp = new Comparator<Item>() {
-                                @Override
-                                public int compare(Item o1, Item o2) {
-                                    float[] result1 = new float[3];
-                                    android.location.Location.distanceBetween(final_lat, final_lon, o1.getLatitude(), o1.getLongitude(), result1);
-                                    Float distance1 = result1[0];
-
-                                    float[] result2 = new float[3];
-                                    android.location.Location.distanceBetween(final_lat, final_lon, o2.getLatitude(), o2.getLongitude(), result2);
-                                    Float distance2 = result2[0];
-                                    return distance1.compareTo(distance2);
-                                }
-                            };
-                            Collections.sort(items_list, comp);
-
-                            for (int i = 0; i < items_list.size(); i++) {
-                                LinearLayout item = new LinearLayout(getBaseContext());
-                                ImageView imageView = new ImageView(getBaseContext());
-                                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(180, 180);
-                                params.setMargins(20, 20, 0, 20);
-                                imageView.setLayoutParams(params);
-                                item.addView(imageView);
-                                //todo: set image
-
-
-
-                                imageView.setImageResource(R.mipmap.poi_test_src);
-                                params = new LinearLayout.LayoutParams(300, 180);
-                                params.setMargins(20, 20, 0, 20);
-                                TextView tv = new TextView(getBaseContext());
-                                //todo : get item info from backend
-                                final Item current_item = items_list.get(i);
-                                String text = "\n" + current_item.getTitle() + "\n" + current_item.getPrice() + "\n" + current_item.getSeller_name();
-                                tv.setText(text);
-                                item.addView(tv);
-                                item.setOnClickListener(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View v) {
-                                        //todo: redirect to related item page
-                                        SharedPreferences.Editor edit = shared.edit();
-                                        edit.putString("itemid", current_item.getid());
-                                        edit.apply();
-                                        Intent intent = new Intent(NearbyItem.this, ItemDetail.class);
-                                        startActivity(intent);
-
+            db.collection("items")
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                ArrayList<Item> items_list = new ArrayList<>();
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    Log.d(TAG, document.getId() + " => " + document.getData());
+                                    Item item = document.toObject(Item.class);
+                                    if (item.getLatitude() != 0 && item.getLongitude() != 0 && item.getStatus() == 1) {
+                                        items_list.add(item);
                                     }
-                                });
-                                items.addView(item);
+                                }
+
+                                Comparator comp = new Comparator<Item>() {
+                                    @Override
+                                    public int compare(Item o1, Item o2) {
+                                        float[] result1 = new float[3];
+                                        android.location.Location.distanceBetween(final_lat, final_lon, o1.getLatitude(), o1.getLongitude(), result1);
+                                        Float distance1 = result1[0];
+
+                                        float[] result2 = new float[3];
+                                        android.location.Location.distanceBetween(final_lat, final_lon, o2.getLatitude(), o2.getLongitude(), result2);
+                                        Float distance2 = result2[0];
+                                        return distance1.compareTo(distance2);
+                                    }
+                                };
+                                Collections.sort(items_list, comp);
+
+                                for (int i = 0; i < items_list.size(); i++) {
+                                    LinearLayout item = new LinearLayout(getBaseContext());
+                                    ImageView imageView = new ImageView(getBaseContext());
+                                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(180, 180);
+                                    params.setMargins(20, 20, 0, 20);
+                                    imageView.setLayoutParams(params);
+                                    item.addView(imageView);
+                                    //todo: set image
+
+
+                                    imageView.setImageResource(R.mipmap.poi_test_src);
+                                    params = new LinearLayout.LayoutParams(300, 180);
+                                    params.setMargins(20, 20, 0, 20);
+                                    TextView tv = new TextView(getBaseContext());
+                                    //todo : get item info from backend
+                                    final Item current_item = items_list.get(i);
+                                    String text = "\n" + current_item.getTitle() + "\n" + current_item.getPrice() + "\n" + current_item.getSeller_name();
+                                    tv.setText(text);
+                                    item.addView(tv);
+                                    item.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            //todo: redirect to related item page
+                                            SharedPreferences.Editor edit = shared.edit();
+                                            edit.putString("itemid", current_item.getid());
+                                            edit.apply();
+                                            Intent intent = new Intent(NearbyItem.this, ItemDetail.class);
+                                            startActivity(intent);
+
+                                        }
+                                    });
+                                    items.addView(item);
+                                }
+                            } else {
+                                Log.d(TAG, "Error getting documents: ", task.getException());
                             }
-                        } else {
-                            Log.d(TAG, "Error getting documents: ", task.getException());
                         }
-                    }
-                });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                    });
+        }
 
     }
     private Location getLastBestLocation() {

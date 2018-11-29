2cf92f3a4113b07577bcbea32f342733ceb83609
Yuening Xu <xu_830@outlook.com>
2018-11-29 18:17:34 -0500

    fix listener in sold notification

--- app/src/main/java/com/example/tongan/unitrade/Paypal.java
+++ app/src/main/java/com/example/tongan/unitrade/Paypal.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,11 +16,16 @@ import android.widget.Toast;
 
 import com.example.tongan.unitrade.Config.Config;
 import com.example.tongan.unitrade.objects.Item;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.paypal.android.sdk.payments.PayPalConfiguration;
 import com.paypal.android.sdk.payments.PayPalPayment;
 import com.paypal.android.sdk.payments.PayPalService;
@@ -150,6 +157,25 @@ public class Paypal extends AppCompatActivity {
                                 }
                             }
                         });
+
+                        Query query =  db.collection("orders").whereEqualTo("item_ID",itemid);
+                        db.collection("orders")
+                                .whereEqualTo("item_ID", itemid)
+                                .get()
+                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                                final DocumentReference order_doc = db.collection("orders").document(document.getId());
+                                                order_doc.update("is_shipped",true);
+                                            }
+                                        } else {
+                                            Log.d(TAG, "Error getting documents: ", task.getException());
+                                        }
+                                    }
+                                });
                     } catch (JSONException e){
                         e.printStackTrace();
                     }

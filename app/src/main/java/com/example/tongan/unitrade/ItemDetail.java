a8ff36a300e9bf194c672e0402e9012614898c81
Yancheng Guo <guo361@purdue.edu>
2018-11-29 02:41:16 -0500

    fix ItemDetail page layout.Tracking number works fine now.

--- app/src/main/java/com/example/tongan/unitrade/ItemDetail.java
+++ app/src/main/java/com/example/tongan/unitrade/ItemDetail.java
@@ -140,7 +140,7 @@ public class ItemDetail extends AppCompatActivity {
                                                         Log.d(TAG, document.getId() + " => " + document.getData());
                                                         final DocumentReference order_doc = db.collection("orders").document(document.getId());
                                                         order_doc.update("is_shipped",true);
-                                                        order_doc.update("trackingnumber",tracking_number);
+                                                        order_doc.update("trackingnumber","tracking_number");
                                                     }
                                                 } else {
                                                     Log.d(TAG, "Error getting documents: ", task.getException());
@@ -254,7 +254,11 @@ public class ItemDetail extends AppCompatActivity {
                 price_edit.setText(temp);
 
                 //set tracking number
-                trackingnumber.setText(track);
+                trackingnumber.setText( track);
+                if (track != null && !track.equals("")){
+                    tracking_number.setVisibility(View.INVISIBLE);
+
+                }
 
                 //Set Item image to one uploaded with the post, if it exists
                 StorageReference storageRef = storage.getReference();

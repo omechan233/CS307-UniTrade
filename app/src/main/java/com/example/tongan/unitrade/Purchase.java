c801a88dd4e249d79edf16fc649221c445c1d45b
Scott Merritt <smerrit@purdue.edu>
2018-12-03 01:07:47 -0500

    fixed image functionaliy with order lists and order details

--- app/src/main/java/com/example/tongan/unitrade/Purchase.java
+++ app/src/main/java/com/example/tongan/unitrade/Purchase.java
@@ -70,7 +70,7 @@ public class Purchase extends AppCompatActivity {
                         else {
                             if(finalFace_to_face) {
                                 Timestamp orderTime = Timestamp.now();
-                                f.create_order(useremail, item.getSeller_name(), itemid, orderTime, item.getPrice(), item.getTitle(), finalFace_to_face, 0,false);
+                                f.create_order(useremail, item.getSeller_name(), itemid, orderTime, item.getPrice(), item.getTitle(), finalFace_to_face, 0,false, item.getItem_image());
                                 f.changeItemStatusToBought(itemid);
                                 Toast.makeText(Purchase.this, "Submit Success! You choose " + radioButton.getText(), Toast.LENGTH_LONG).show();
                             }
